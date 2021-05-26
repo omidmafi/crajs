e7a7dd4361bfaf5dbab42e1829a02f5d4ca0b007
Omid Mafi <omidmafi@gmail.com>
2021-05-26 13:37:49 -0400

    My change

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,9 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toHaveStyle({color: 'blue'});
+
+  // const link = document.getElementsByClassName('App-link')[0];
+  // const style = window.getComputedStyle(link);  
+  // console.log(style);
 });
